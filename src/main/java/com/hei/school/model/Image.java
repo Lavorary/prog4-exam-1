8a2926d4d28398e944de9079f4a9ad47889e6b83
Lavorary <hei.lavorary@gmail.com>
2026-07-13 08:37:03 +0300

    feat : add image model

--- src/main/java/com/hei/school/model/Image.java
+++ src/main/java/com/hei/school/model/Image.java
@@ -1,9 +1,8 @@
 package com.hei.school.model;
 
-import lombok.Builder;
-
 import java.time.Instant;
 import java.util.UUID;
+import lombok.Builder;
 
 @Builder
-public record Image(UUID id, String filename,String email, Instant createdAt) {}
+public record Image(UUID id, String filename, String email, Instant createdAt) {}
--- src/main/java/com/hei/school/repository/model/JImage.java
+++ src/main/java/com/hei/school/repository/model/JImage.java
@@ -1,12 +1,11 @@
 package com.hei.school.repository.model;
 
+import jakarta.persistence.*;
+import java.time.Instant;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import jakarta.persistence.*;
-
-import java.time.Instant;
-import java.util.UUID;
 
 @Entity
 @Data
@@ -14,16 +13,14 @@ import java.util.UUID;
 @NoArgsConstructor
 @Table(name = "image")
 public class JImage {
-    @Id
-    private UUID id;
+  @Id private UUID id;
 
-    @Column(nullable = false)
-    private String filename;
+  @Column(nullable = false)
+  private String filename;
 
-    @Column(nullable = false)
-    private String email;
+  @Column(nullable = false)
+  private String email;
 
-    @Column(nullable = false)
-    private Instant createdAt;
-
+  @Column(nullable = false)
+  private Instant createdAt;
 }
